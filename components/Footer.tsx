ae65cb0edbcddabcc20ef508fa23368e26b5fe57
Mert Topkaya <36225491+plastikmind@users.noreply.github.com>
2024-02-06 11:16:36 +0300

    small update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col items-center justify-center mt-20 md:mt-40 mb-12'>
-       <Link href='/' className='relative w-[160px] h-[197px] sm:w-[250px] sm:h-[308px] mt-5'>
-       <Image src='/vero-logo-yellow.png'  alt='logo' layout='fill' objectFit='fill' />
-       </Link>
-       <Link href='https://studiomuz.com' target='blank'>
-       <p className='pt-8 text-lg items-center text-center font-extralight'>Made with love by <span className='text-3xl'>🍌</span> </p>
-       
-       </Link>
+    <div className="flex flex-col items-center justify-center mt-20 md:mt-40 mb-12">
+      <Link
+        href="/"
+        className="relative w-[160px] h-[197px] sm:w-[250px] sm:h-[308px] mt-5"
+      >
+        <Image
+          src="/vero-logo-yellow.png"
+          alt="logo"
+          layout="fill"
+          objectFit="fill"
+        />
+      </Link>
+      <Link href="https://studiomuz.com" target="blank">
+        <p className="pt-8 text-lg items-center text-center font-extralight hover:text-gray-500">
+          Made with love by <span className="text-3xl">🍌</span>{" "}
+        </p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
